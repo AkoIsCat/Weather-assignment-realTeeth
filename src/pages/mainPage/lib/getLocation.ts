8da2ea68b9e2b3cf27e52d553185f1abbc31c7a1
KimEunHye <gpqjr00@naver.com>
2026-01-10 02:54:53 +0900

    Feat: 사용자 위치 정보를 가져오는 유틸함수 작성

---
+++ src/pages/mainPage/lib/getLocation.ts
@@ -0,0 +1,15 @@
+export const getLocation = () => {
+  return new Promise<[number, number]>((res, rej) => {
+    if (!navigator.geolocation) {
+      rej(new Error('NOT Supported'));
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        res([position.coords.latitude, position.coords.longitude]);
+      },
+      (error) => rej(error)
+    );
+  });
+};
