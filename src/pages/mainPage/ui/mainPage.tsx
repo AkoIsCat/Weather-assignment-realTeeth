70df48a3d6b755376ea78430eae97b32e4d8744a
KimEunHye <gpqjr00@naver.com>
2026-01-10 02:56:08 +0900

    Feat: 메인페이지에서 주소정보를 가져오는 useEffect 작성

--- src/pages/mainPage/ui/mainPage.tsx
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -1,6 +1,13 @@
 import { Header } from '../../../widgets/Header';
+import { useEffect } from 'react';
+import { getLocation } from '../lib/getLocation';
+import { getAddress } from '../lib/getAddress';
 
 export const MainPage = () => {
+  useEffect(() => {
+    getLocation().then((data) => getAddress(data));
+  }, []);
+
   return (
     <div>
       <Header />
