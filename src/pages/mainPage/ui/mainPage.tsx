99ff62d5596db3e19ca37644e406edd008ab639c
KimEunHye <gpqjr00@naver.com>
2026-01-11 03:24:22 +0900

    Feat: 사용자 현재 위치 감지 기능 구현

--- src/pages/mainPage/ui/mainPage.tsx
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -1,16 +1,26 @@
 import { Header } from '../../../widgets/Header';
-import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { getLocation } from '../lib/getLocation';
 import { getAddress } from '../lib/getAddress';
 
 export const MainPage = () => {
-  useEffect(() => {
-    getLocation().then((data) => getAddress(data));
-  }, []);
+  const { data: address } = useQuery({
+    queryKey: ['address'],
+    queryFn: async () => {
+      const coords = await getLocation();
+      return getAddress(coords);
+    },
+    select: (data) => data[0],
+  });
+
+  console.log(address);
 
   return (
     <div>
-      <Header />
+      <Header
+        district={address?.region_2depth_name ?? ''}
+        neighborhood={address?.region_3depth_name ?? ''}
+      />
     </div>
   );
 };
