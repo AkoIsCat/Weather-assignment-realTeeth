f3a6bcd61c9ee345b0f29e8bccbd73be57f72602
KimEunHye <gpqjr00@naver.com>
2026-01-12 05:03:01 +0900

    Design: 모바일 환경 레이아웃 작성

--- src/pages/mainPage/ui/mainPage.tsx
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -1,26 +1,48 @@
 import { Header } from '../../../widgets/Header';
-import { useQuery } from '@tanstack/react-query';
-import { getLocation } from '../lib/getLocation';
-import { getAddress } from '../lib/getAddress';
+import { useCoords } from '../../../entities/coords';
+import { useAddress } from '../../../entities/address';
+import { useForecast, useWeather } from '../../../entities/weather';
+import { WeatherIcon } from './WeatherIcon';
 
 export const MainPage = () => {
-  const { data: address } = useQuery({
-    queryKey: ['address'],
-    queryFn: async () => {
-      const coords = await getLocation();
-      return getAddress(coords);
-    },
-    select: (data) => data[0],
-  });
+  const coords = useCoords();
+  const address = useAddress(coords);
+  const weather = useWeather(coords);
+  const forecast = useForecast(coords);
 
-  console.log(address);
+  console.log(weather, forecast);
 
   return (
-    <div>
+    <div className="w-screen h-screen flex flex-col items-center ">
       <Header
         district={address?.region_2depth_name ?? ''}
         neighborhood={address?.region_3depth_name ?? ''}
+        village={address?.region_4depth_name ?? ''}
       />
+      <main className="w-screen">
+        {/* 검색 영역 */}
+        <section className="bg-amber-500 h-12">Search</section>
+        {/* 현재 날씨 영역 */}
+        <section className="bg-pink-400 h-70">
+          Current Weather
+          {/* <div>
+            <WeatherIcon
+              icon={weather?.weather[0].icon}
+              description={weather?.weather[0].description}
+            />
+          </div>
+          <h1 className="-mt-10">{Math.floor(weather?.main.temp)}°</h1>
+          <div className="flex gap-2">
+            <span>최고 {weather?.main.temp_max}°</span>
+            <span>|</span>
+            <span>최저 {weather?.main.temp_min}°</span>
+          </div> */}
+        </section>
+        {/* 시간대 별 기온 영역 */}
+        <section className="bg-sky-400 h-50">Hourly Weather</section>
+        {/* 즐겨찾기 영역 */}
+        <section className="bg-green-400 h-40">favorite</section>
+      </main>
     </div>
   );
 };
--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,9 +1,15 @@
-import { LocationIcon } from '../../../shared';
+// import { LocationIcon } from '../../../shared';
+import type { HeaderProps } from '../model/types';
 
-export const Header = () => {
+export const Header = ({ district, neighborhood, village }: HeaderProps) => {
+  // 데이터가 아직 들어오지 않았으면 로딩중이라고 표시
+  // const displayDistrict = district || '위치 정보';
+  // const displayNeighborhood = neighborhood || '로딩 중';
+  // const displayVillage = village || '...';
+
   return (
-    <div>
-      <LocationIcon />
-    </div>
+    <header className="bg-red-400 w-full h-14 px-2">
+      <div className="bg-blue-400 h-full">Header</div>
+    </header>
   );
 };
