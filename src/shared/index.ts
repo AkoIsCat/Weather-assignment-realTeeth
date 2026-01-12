718ed2d586935c66c6b6437a3f75cff7ec164d39
KimEunHye <gpqjr00@naver.com>
2026-01-13 04:24:11 +0900

    Feat: 검색, 구름 아이콘 svg 추가

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,9 +1,13 @@
 // ./ui
-export * from './ui/Card';
-export * from './ui/CurrentLocation';
-export * from './ui/MaxTmpr';
-export * from './ui/MinTmpr';
-export * from './ui/Temperature';
+export { Card } from './ui/Card';
+export { CurrentLocation } from './ui/CurrentLocation';
+export { MaxTmpr } from './ui/MaxTmpr';
+export { MinTmpr } from './ui/MinTmpr';
+export { Temperature } from './ui/Temperature';
+export { Input } from './ui/Input';
+export { Logo } from './ui/Logo';
 
 // ./asset
 export { default as LocationIcon } from './asset/location.svg?react';
+export { default as SearchIcon } from './asset/search.svg?react';
+export { default as CloudIcon } from './asset/cloud.svg?react';
