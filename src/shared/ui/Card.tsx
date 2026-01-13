40de9d0a12a39eb827e51c05db559bc2cfee433b
KimEunHye <gpqjr00@naver.com>
2026-01-13 18:01:21 +0900

    Feat: 최저/최고 기온 Card width 추가

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -4,18 +4,22 @@ export const Card = ({ children, width = 'mobile' }: CardProps) => {
   // width 동적css
   const widthClass: Record<CardWidth, string> = {
     // 모바일: 1열 100%
-    mobile: 'w-full',
+    mobile: 'w-full min-w-0',
 
-    // 데스크탑: 12컬럼 그리드 기준 (5/7/12)
-    currentDesktop: 'w-full lg:col-span-5',
-    hourlyDesktop: 'w-full lg:col-span-7',
-    favoriteDesktop: 'w-full lg:col-span-12',
+    // 데스크탑: 12컬럼 그리드 기준 (4/8/12)
+    currentDesktop: 'w-full lg:col-span-4 min-w-0',
+    hourlyDesktop: 'w-full lg:col-span-8 min-w-0',
+    favoriteDesktop: 'w-full lg:col-span-12 min-w-0',
 
     // 즐겨찾기 지역 카드
     favoriteItem: 'w-full',
 
     // 시간대별 기온 카드
-    hourlyItem: 'w-[112px] shrink-0 lg:w-[120px]',
+    hourlyItem: 'w-[112px] lg:w-[120px] min-w-0',
+
+    // 최고 / 최저 기온 카드
+    tempStatItem:
+      'w-[125px] lg:w-[140px] h-[30px] min-w-0 flex justify-center items-center gap-3',
   };
 
   return (
