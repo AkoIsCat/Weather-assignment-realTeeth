754c6663736557255a10fc440ad6bb941e5a3c96
KimEunHye <gpqjr00@naver.com>
2026-01-13 18:01:38 +0900

    Refactor: 불필요한 export 제거

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,13 +1,13 @@
 // ./ui
 export { Card } from './ui/Card';
-export { CurrentLocation } from './ui/CurrentLocation';
-export { MaxTmpr } from './ui/MaxTmpr';
-export { MinTmpr } from './ui/MinTmpr';
-export { Temperature } from './ui/Temperature';
 export { Input } from './ui/Input';
 export { Logo } from './ui/Logo';
+export { WeatherIcon } from './ui/WeatherIcon';
 
 // ./asset
 export { default as LocationIcon } from './asset/location.svg?react';
 export { default as SearchIcon } from './asset/search.svg?react';
 export { default as CloudIcon } from './asset/cloud.svg?react';
+
+// ./types
+export type { WeatherIconType } from './model/types';
