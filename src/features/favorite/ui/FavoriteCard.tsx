dd1e5fc8215df0a251ff9a694b6fb0e45d5e3c9d
KimEunHye <gpqjr00@naver.com>
2026-01-14 04:37:50 +0900

    Feat: 즐겨찾기 카드 아이템 컴포넌트 작성중

---
+++ src/features/favorite/ui/FavoriteCard.tsx
@@ -0,0 +1,31 @@
+import { WeatherIcon } from '../../../shared';
+import { CurrentTmp, TodayTmp } from '../../../entities/weather';
+import { Card } from '../../../shared';
+
+// 임시 타입. 나중에 실제 데이터에 맞춰서 바꿀것.
+type PropsType = {
+  city: string;
+  curTmp: number;
+  minTmp: number;
+  maxTmp: number;
+};
+
+export const FavoriteCard = ({ city, curTmp, minTmp, maxTmp }: PropsType) => {
+  return (
+    <Card width="favoriteCard">
+      <div className="flex">
+        <div>
+          <p>{city}</p>
+          <CurrentTmp curTmp={curTmp} />
+        </div>
+        {/* <WeatherIcon icon="icon" description="dummy" /> */}
+      </div>
+      <div className="flex gap-2 text-xs">
+        <span>최고 {maxTmp} |</span>
+        <span>최저 {minTmp}</span>
+        {/* <TodayTmp type="max" minTmp={maxTmp} />
+        <TodayTmp type="min" minTmp={minTmp} /> */}
+      </div>
+    </Card>
+  );
+};
