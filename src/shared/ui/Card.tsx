4c66ced7507d419f918422b2c2171c6ac1cfe5e0
KimEunHye <gpqjr00@naver.com>
2026-01-15 03:36:15 +0900

    Design: 즐겨찾기 카드 cursor-pointer 적용

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -18,7 +18,8 @@ export const Card = ({
     // 즐겨찾기 지역 카드
     favoriteItem: 'w-full p-5 lg:p-6',
     // 즐겨찾기 카드 아이템
-    favoriteCard: 'w-[calc(50%-0.5rem)]  lg:w-[calc(16%-0.5rem)] p-5 lg:p-6',
+    favoriteCard:
+      'w-[calc(50%-0.5rem)]  lg:w-[calc(16%-0.5rem)] p-5 lg:p-6  cursor-pointer',
 
     // 시간대별 기온 카드
     hourlyItem: 'w-[80px] lg:w-[90px] min-w-0 min-h-30',
