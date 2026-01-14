f4891f9bd33444a58c06bd098cd9c6ca8e5417c2
KimEunHye <gpqjr00@naver.com>
2026-01-15 01:18:30 +0900

    Feat: json파일 export

--- src/entities/address/index.ts
+++ src/entities/address/index.ts
@@ -1,3 +1,6 @@
 export { useAddress } from './model/useAddress';
 
 export { getAddress } from './api/getAddress';
+
+import korea_districts from './model/korea_districts.json';
+export { korea_districts };
