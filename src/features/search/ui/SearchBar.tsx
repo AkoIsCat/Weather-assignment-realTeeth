58cd0f343410c981067b204f806a082fb99d1399
KimEunHye <gpqjr00@naver.com>
2026-01-15 01:25:36 +0900

    Feat: 사용자가 검색한 내용을 보여주고 json 목록 중에서 일치하는걸 필터링하도록 구현

--- src/features/search/ui/SearchBar.tsx
+++ src/features/search/ui/SearchBar.tsx
@@ -1,13 +1,34 @@
-import { Input } from '../../../shared';
 import { SearchIcon } from '../../../shared';
+import { SearchSuggestionList } from './SearchSuggestionList';
+import { useState } from 'react';
+import { korea_districts } from '../../../entities/address';
+import { SearchInput } from '../../../entities/search/ui/SearchInput';
 
 export const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const isValidSearch = searchValue.trim().length > 0;
+
+  const filterLocation = korea_districts.filter((item) =>
+    item.includes(searchValue)
+  );
+
   return (
     <div className="relative flex items-center z-0 px-4">
       <div className="absolute z-10 left-7">
         <SearchIcon />
       </div>
-      <Input />
+      <div className="relative w-full">
+        <SearchInput
+          value={searchValue}
+          setValue={setSearchValue}
+          isValidSearch={isValidSearch}
+        />
+        {isValidSearch && (
+          <div className="absolute left-0 right-0 top-full z-20">
+            <SearchSuggestionList data={filterLocation} />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
