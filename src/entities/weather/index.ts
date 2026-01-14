01f8e51e7bd9b7a7ab3a1e2b32f8bbc9b25dcdf1
KimEunHye <gpqjr00@naver.com>
2026-01-15 06:08:25 +0900

    Feat: 타입 및 유틸함수 export

--- src/entities/weather/index.ts
+++ src/entities/weather/index.ts
@@ -5,7 +5,12 @@ export { getWeather } from './api/getWeather';
 export { useWeather } from './model/useWeather';
 export { useForecast } from './model/useForecast';
 export { useWeatherStore } from './model/useWeatherStore';
+export type { WeatherData } from './model/types';
 
 // ./weather/ui
 export { TodayTmp } from './ui/TodayTmp';
 export { CurrentTmp } from './ui/CurrentTmp';
+
+// ./weather/lib
+export { filterWeatherData } from './lib/filterWeatherData';
+export { formatToKST } from './lib/formatToKST';
