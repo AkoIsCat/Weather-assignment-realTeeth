fb76aed58b4f6960e1381758d745e25370aa99f3
KimEunHye <gpqjr00@naver.com>
2026-01-15 05:06:21 +0900

    Feat: 드롭다운메뉴 바깥 영역을 클릭 할 경우 드롭다운 메뉴가 닫히도록 구현

--- src/features/search/ui/SearchBar.tsx
+++ src/features/search/ui/SearchBar.tsx
@@ -1,19 +1,37 @@
 import { SearchIcon } from '../../../shared';
 import { SearchSuggestionList } from './SearchSuggestionList';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { korea_districts } from '../../../entities/address';
 import { SearchInput } from '../../../entities/search/ui/SearchInput';
 
 export const SearchBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const [searchValue, setSearchValue] = useState<string>('');
-  const isValidSearch = searchValue.trim().length > 0;
+  const itemRef = useRef<HTMLDivElement>(null);
 
+  const isValidSearch = searchValue.trim().length > 0;
   const filterLocation = korea_districts.filter((item) =>
     item.includes(searchValue)
   );
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // 클릭된 요소가 itemRef 내부가 아니라면 닫음
+      if (itemRef.current && !itemRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    // 전역 document에 이벤트 리스너 등록
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // 컴포넌트 언마운트 시 리스너 제거 
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative flex items-center z-0 px-4">
+    <div ref={itemRef} className="relative flex items-center z-0 px-4">
       <div className="absolute z-10 left-7">
         <SearchIcon />
       </div>
@@ -21,9 +39,10 @@ export const SearchBar = () => {
         <SearchInput
           value={searchValue}
           setValue={setSearchValue}
-          isValidSearch={isValidSearch}
+          setIsOpen={setIsOpen}
+          isValidSearch={isOpen}
         />
-        {isValidSearch && (
+        {isValidSearch && isOpen && (
           <div className="absolute left-0 right-0 top-full z-20">
             <SearchSuggestionList data={filterLocation} />
           </div>
