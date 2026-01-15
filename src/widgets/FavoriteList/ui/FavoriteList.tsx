737be12a636d792a8cf4955f14ba22d081564758
KimEunHye <gpqjr00@naver.com>
2026-01-16 02:54:16 +0900

    Feat: 즐겨찾기 지역이 없을 시 문구 표시

--- src/widgets/FavoriteList/ui/FavoriteList.tsx
+++ src/widgets/FavoriteList/ui/FavoriteList.tsx
@@ -1,8 +1,16 @@
 import { FavoriteCard } from '../../../features/favorite';
 import { useFavoriteStore } from '../../../features/favorite';
+import { Card } from '../../../shared';
 
 export const FavoriteList = () => {
   const { favoriteLocationList } = useFavoriteStore();
+
+  if (favoriteLocationList.length === 0) {
+    return (
+      <Card width="mobile">자주 확인하는 지역을 즐겨찾기에 추가해보세요!</Card>
+    );
+  }
+
   return favoriteLocationList.map((item) => (
     <FavoriteCard
       key={item.location}
