92b4db53eeab1754662ff9557db7c2dc6b95510a
KimEunHye <gpqjr00@naver.com>
2026-01-16 04:26:28 +0900

    Design: 시간별기온 가로스크롤 활성화

--- src/widgets/HourlyWeather/ui/HourlyWeatherList.tsx
+++ src/widgets/HourlyWeather/ui/HourlyWeatherList.tsx
@@ -2,7 +2,7 @@ import { HourlyWeather, type WeatherData } from '../../../entities/weather';
 
 export const HourlyWeatherList = ({ data }: { data: WeatherData[] }) => {
   return (
-    <div className="flex overflow-x-auto gap-5 lg:gap-10 scrollbar-hide px-6 pb-12">
+    <div className="flex overflow-x-auto gap-5 lg:gap-10 px-6 pb-12">
       {data?.map((item) => (
         <HourlyWeather key={item.dt} data={item} />
       ))}
