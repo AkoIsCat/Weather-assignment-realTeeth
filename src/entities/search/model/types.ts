5404d3f054c16fdafc0d83b602acd3863faa8d07
KimEunHye <gpqjr00@naver.com>
2026-01-15 23:17:42 +0900

    Fix: useWeatherStore 경로 수정

--- src/entities/search/model/types.ts
+++ src/entities/search/model/types.ts
@@ -1,11 +1,3 @@
-export type WeatherState = {
-  currentLocation: string;
-};
-
-export type WeatherAction = {
-  setCurrentLocation: (location: string) => void;
-};
-
 export type SuggestionItemType = {
   location: string;
   onClick: (location: string) => void;
