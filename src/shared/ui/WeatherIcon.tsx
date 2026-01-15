0cd414684dcea7bf0cb904f515216448e841bf82
KimEunHye <gpqjr00@naver.com>
2026-01-15 16:48:32 +0900

    Design: 용도에 따른 날씨 이미지 크기 설정

--- src/shared/ui/WeatherIcon.tsx
+++ src/shared/ui/WeatherIcon.tsx
@@ -1,14 +1,20 @@
 import type { WeatherIconType } from '../model/types';
 
-export const WeatherIcon = ({ icon, description }: WeatherIconType) => {
+export const WeatherIcon = ({ icon, description, width }: WeatherIconType) => {
   const iconUrl = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+
+  const widthCss = {
+    hourly: 'w-[110px] h-[120px]',
+    favorite: '',
+    current: 'w-full h-full',
+  };
 
   return (
     <div className="w-50 flex items-center justify-center leading-none">
       <img
         src={iconUrl}
         alt={description}
-        className="block w-full h-full object-contain -mb-4"
+        className={`block ${widthCss[width]} object-contain -mb-4`}
       />
     </div>
   );
