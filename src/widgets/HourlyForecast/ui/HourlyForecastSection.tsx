75fdce3e98d582e54a8b414f2aee3a0310fd8107
KimEunHye <gpqjr00@naver.com>
2026-01-15 16:51:30 +0900

    Feat: 시간별기온 더미데이터 삭제

--- src/widgets/HourlyForecast/ui/HourlyForecastSection.tsx
+++ src/widgets/HourlyForecast/ui/HourlyForecastSection.tsx
@@ -1,45 +1,13 @@
+import type { WeatherData } from '../../../entities/weather';
 import { Card } from '../../../shared';
 import { HourlyForecastList } from './HourlyForecastList';
 
-const dummy = [
-  {
-    time: 12,
-    temp: 5,
-    icon: '',
-  },
-  {
-    time: 15,
-    temp: 7,
-    icon: '',
-  },
-  {
-    time: 18,
-    temp: 1,
-    icon: '',
-  },
-  {
-    time: 21,
-    temp: -6,
-    icon: '',
-  },
-  {
-    time: 24,
-    temp: -10,
-    icon: '',
-  },
-  {
-    time: 3,
-    temp: -10,
-    icon: '',
-  },
-];
-
-export const HourlyForecastSection = () => {
+export const HourlyForecastSection = ({ data }: { data: WeatherData[] }) => {
   return (
-    <section className="lg:col-span-7 px-4 lg:pr-10">
+    <section className="lg:col-span-8 px-4 lg:pr-10">
       <Card width="hourlyDesktop">
         <div className="px-6 pb-4 pt-6">시간대 별 날씨</div>
-        <HourlyForecastList data={dummy} />
+        <HourlyForecastList data={data} />
       </Card>
     </section>
   );
