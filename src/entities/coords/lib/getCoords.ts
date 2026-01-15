61da6fcdfcf522c4e6b3b9eb2d8b4dddce97cf7a
KimEunHye <gpqjr00@naver.com>
2026-01-15 16:45:02 +0900

    Feat: 위치정보를 불러올 수 없을시 초기 지역을 강남구로 설정

--- src/entities/coords/lib/getCoords.ts
+++ src/entities/coords/lib/getCoords.ts
@@ -1,7 +1,7 @@
 export const getCoords = () => {
-  return new Promise<[number, number]>((res, rej) => {
+  return new Promise<[number, number]>((res) => {
     if (!navigator.geolocation) {
-      rej(new Error('NOT Supported'));
+      res([37.4979, 127.0276]); // 브라우저가 지원하지 않는 경우 초기값 강남구
       return;
     }
 
@@ -9,7 +9,17 @@ export const getCoords = () => {
       (position) => {
         res([position.coords.latitude, position.coords.longitude]);
       },
-      (error) => rej(error)
+      (error) => {
+        console.warn(
+          '위치 정보를 가져올 수 없어 강남구 좌표를 반환합니다.',
+          error.message
+        );
+        res([37.4979, 127.0276]);
+      },
+      {
+        timeout: 5000, // 5초 안에 응답 없으면 에러로 간주
+        maximumAge: Infinity, // 캐시된 위치 정보 허용
+      }
     );
   });
 };
